00091cde8ea1216f3a5627704100480040a13b35
Kaikyl <kaikylimasouza@gmail.com>
2024-03-15 16:14:37 -0300

    update 1

--- models/Operador.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import Integer, VARCHAR, ForeignKey
-from models import Base, Funcionario
-
-class Operador(Base):
-    _tablename_ = "Operador"
-    idOperador:            Mapped[int] = mapped_column(Integer, nullable=False, autoincrement=True, primary_key=True)
-    nmOperador:            Mapped[str] = mapped_column(VARCHAR(100), nullable=False)
-    funcionarioNrMatricula:Mapped[int] = mapped_column(Integer, ForeignKey(Funcionario.nrMatricula))
