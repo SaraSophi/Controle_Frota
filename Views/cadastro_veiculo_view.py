953a403cc9f8ef49907cdd8ec4f2fd9180d3a281
Kaikyl <kaikylimasouza@gmail.com>
2024-03-26 10:37:37 -0300

    validar ano veic / ano aquisicao

--- Views/cadastro_veiculo_view.py
+++ Views/cadastro_veiculo_view.py
@@ -3,6 +3,7 @@ from services.db import session
 from models.Veiculo_model import Veiculo
 from PyQt5.QtWidgets import QMessageBox
 from sqlalchemy.exc import IntegrityError
+import re
 from datetime import datetime
 
 class CadastroVeiculoView(QtWidgets.QMainWindow):
@@ -26,7 +27,6 @@ class CadastroVeiculoView(QtWidgets.QMainWindow):
 
     def registro_veiculo(self):
         try:
-
             nrFrota         = self.nrFrota.text()
             nrConjunto      = self.nrConjunto.text()
             nrPlaca         = self.nrPlaca.text()
@@ -36,12 +36,29 @@ class CadastroVeiculoView(QtWidgets.QMainWindow):
             dsMarca         = self.dsMarca.currentText()
             tpVeiculo       = self.tpVeiculo.currentText()
             tpCombustivel   = self.tpCombustivel.currentText()
-            qtEixo          = self.qtdEixo.text()  # Convertido para inteiro
+            qtEixo          = int(self.qtdEixo.text())  # Convertido para inteiro
             nrChassi        = self.nrChassi.text()
             tpTracao        = self.tpTracao.currentText()
             dtAquisicao     = self.dtAquisicao.date().toPyDate()  # Mantido como objeto datetime
 
-            #veiculo_idVeiculo = idVeic   VEICULO_IDVEICULO=veiculo_idVeiculo
+            #Validações para cadastro efetivo no banco
+            if not self.validar_placa(nrPlaca):
+                QMessageBox.critical(self, "Erro", "Placa inválida!")
+                return
+            if not self.validar_renavam(nrRenavam):
+                QMessageBox.critical(self, "Erro", "Renavam inválido!")
+                return
+            if not self.valida_chassi(nrChassi):
+                QMessageBox.critical(self, "Erro", "Chassi inválido!")
+                return
+            if not self.valida_ano(anoVeic, dtAquisicao):
+                QMessageBox.critical(self, "Erro", "O ano de fabricação do veículo não pode ser supeior a 1 ano a mais da data de aquisição!")
+                return
+
+
+
+
+
             novo_veiculo = Veiculo(NRPLACA=nrPlaca, DSMODELO=dsModelo, TPTRACAO=tpTracao,
                                    NRRENAVAM=nrRenavam, DTAQUISICAO=dtAquisicao, NRFROTA=nrFrota,
                                    NRCONJUNTO=nrConjunto, NRCHASSI=nrChassi, TPCOMBUSTIVEL=tpCombustivel,
@@ -60,9 +77,41 @@ class CadastroVeiculoView(QtWidgets.QMainWindow):
             session.rollback()
             QMessageBox.critical(self,"Erro", f"Ocorreu um erro ao realizar o cadastro: {e}")
 
+
+    def validar_placa(self, nrPlaca):  #Validação da placa para Mercosul ou modelo antigo;
+        padraoTradicional = r'^[A-Za-z]{3}\d{4}$'
+        padraoMercosul = r'^[A-Za-z]{3}\d[A-Za-z]\d{2}$'
+        return re.match(padraoTradicional, nrPlaca) or re.match(padraoMercosul, nrPlaca)
+
+    def validar_renavam(self, nrRenavam):# Validação do renavam
+        renavam = str(nrRenavam).zfill(11)
+        if len(renavam) == 9:
+            renavam = "00" + renavam
+        elif len(renavam) != 11:
+            return False
+
+        d = [int(x) for x in renavam]
+        soma = sum(d[i] * int(peso) for i, peso in enumerate("3298765432"))
+        resto = soma % 11
+        dv = 11 - resto if resto > 1 else 0
+        return dv == d[-1]
+
+    def valida_chassi(self, nrChassi): #Validação do chassi
+        padraoChassi = r'^[A-HJ-NPR-Z0-9]{17}$'
+        return re.match(padraoChassi, nrChassi)
+
+    def valida_ano(self, anoVeic, dtAquisicao): #Verifica se o ano de fabricação de veiculo não é superior a 1 ano a mais do ano de aquisicao
+        anoAquisicao = dtAquisicao.year
+        return anoVeic <= (anoAquisicao + 1)
+
+
+
+
+
+
+
     def limpar_campos(self):
 
-        self.idVeic.clear()
         self.nrFrota.clear()
         self.nrConjunto.clear()
         self.nrPlaca.clear()
@@ -83,3 +132,5 @@ if __name__ == "__main__":
     window = CadastroVeiculoView()
 
     app.exec()
+
+
