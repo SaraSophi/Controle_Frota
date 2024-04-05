78a686fdfb138a09b292c1052931dd66dfde7a3b
Kaikyl <kaikylimasouza@gmail.com>
2024-04-04 22:59:01 -0300

    validacao vinculado

--- Views/vinculo_motorista_veiculo_view.py
+++ Views/vinculo_motorista_veiculo_view.py
@@ -5,6 +5,8 @@ from models.CtVinculo import CtVinculo
 from models.Motorista import Motorista
 from models.Funcionario import Funcionario
 from models.Veiculo_model import Veiculo
+from sqlalchemy import func
+
 
 class DesvincularMotorista(QtWidgets.QMainWindow):
     def __init__(self):
@@ -16,8 +18,8 @@ class DesvincularMotorista(QtWidgets.QMainWindow):
     def desvincular_motorista(self):
         try:
             dtDesvinculo = self.dtDesvinculo.date().toPyDate()
-            nrMatricula  = self.matriculaMotorista.text()
-            nrFrota      = self.nrFrota.text()
+            nrMatricula = self.matriculaMotorista.text()
+            nrFrota = self.nrFrota.text()
 
             # Localize o ID do vinculo com os atributos fornecidos e DTDESVINCULO nulo
             idVinculo = self.localizar_id_vinculo(nrFrota, nrMatricula)
@@ -34,17 +36,17 @@ class DesvincularMotorista(QtWidgets.QMainWindow):
             QMessageBox.critical(self, "Erro", f"Erro ao atualizar data de desengate: {str(e)}")
 
     def localizar_id_vinculo(self, nrFrota, nrMatricula):
-        id_veiculo = session.query(Veiculo.IDVEICULO).filter(Veiculo.NRFROTA == nrFrota).scalar()
-        id_motorista = session.query(Motorista.IDMOTORISTA).join(Funcionario,
-                                                                 Funcionario.NRMATRICULA == Motorista.FUNCIONARIO_NRMATRICULA).filter(
-            Motorista.FUNCIONARIO_NRMATRICULA == nrMatricula).scalar()
+        veiculo = session.query(Veiculo).filter(Veiculo.NRFROTA == nrFrota).first()
+        motorista = session.query(Motorista).join(Funcionario,
+                                                  Funcionario.NRMATRICULA == Motorista.FUNCIONARIO_NRMATRICULA).filter(
+            Motorista.FUNCIONARIO_NRMATRICULA == nrMatricula).first()
 
-        if id_veiculo is None or id_motorista is None:
+        if veiculo is None or motorista is None:
             return None
 
-        vinculos = session.query(CtVinculo.ID).filter(
-            CtVinculo.VEICULO_IDVEICULO == id_veiculo,
-            CtVinculo.MOTORISTA_IDMOTORISTA == id_motorista,
+        vinculos = session.query(CtVinculo).filter(
+            CtVinculo.VEICULO_IDVEICULO == veiculo.IDVEICULO,
+            CtVinculo.MOTORISTA_IDMOTORISTA == motorista.IDMOTORISTA,
             CtVinculo.DTDESVINCULO.is_(None)
         ).all()
 
@@ -68,11 +70,53 @@ class VincularMotorista(QtWidgets.QMainWindow):
     def criando_novo_vinculo(self):
         self.botaoSalvar.clicked.connect(self.criando_vinculo_motorista)
 
+    @staticmethod
+    def procurar_motorista(nrMatricula):
+        response = session.query(Motorista).join(Funcionario,
+                                                 Funcionario.NRMATRICULA == Motorista.FUNCIONARIO_NRMATRICULA).filter(
+            Motorista.FUNCIONARIO_NRMATRICULA == nrMatricula).first()
+        id_motorista = response.IDMOTORISTA
+
+        return id_motorista
+
+    @staticmethod
+    def procurar_frota(nrFrota):
+        response = session.query(Veiculo).filter(Veiculo.NRFROTA == nrFrota).first()
+        id_veiculo = response.IDVEICULO
+        return id_veiculo
+
+    def vinculado_or_no_frota(self, nrFrota):
+        id_veiculo = VincularMotorista.procurar_frota(nrFrota)
+
+        vinculos = session.query(CtVinculo.ID).filter(
+            CtVinculo.VEICULO_IDVEICULO == id_veiculo,
+            CtVinculo.DTDESVINCULO == None
+        ).all()
+
+        print("quantidade achada veiculos: ", vinculos)
+
+        return len(vinculos)
+
+    def vinculado_or_no_motorista(self, nrMatricula):
+        id_motorista = int(VincularMotorista.procurar_motorista(nrMatricula))
+        print(f"\n \n \n ID motorista {id_motorista}")
+        print(f"ID's motorista banco {session.query(CtVinculo.MOTORISTA_IDMOTORISTA)}")
+
+        vinculos = session.query(CtVinculo.ID).filter(
+            CtVinculo.MOTORISTA_IDMOTORISTA == id_motorista,
+            CtVinculo.DTDESVINCULO == None
+        ).all()
+
+        print("quantidade achada motorista: ", len(vinculos))
+        print("Lista de vinculos", vinculos)
+
+        return len(vinculos)
+
     def criando_vinculo_motorista(self):
         try:
-            dtVinculo    = self.dtVinculo.date().toPyDate()
-            nrMatricula  = self.matriculaMotorista.text()
-            nrFrota      = self.nrFrota.text()
+            dtVinculo = self.dtVinculo.date().toPyDate()
+            nrMatricula = self.matriculaMotorista.text()
+            nrFrota = self.nrFrota.text()
             self.desvinculo_view = None
 
             id_motorista = VincularMotorista.procurar_motorista(nrMatricula)
@@ -80,15 +124,17 @@ class VincularMotorista(QtWidgets.QMainWindow):
 
             erros = []
 
-            if not self.verificar_dt_desvinculo_nulo(nrMatricula, nrFrota):
-                erros.append("Motorista / veiculo já vinculado")
-                # Adicione a verificação do campo DTDESVINCULO nulo aqui
-            elif self.verificar_dt_desvinculo_nulo(id_veiculo, id_motorista):
-                erros.append("Motorista possui um vínculo ativo.")
+            if self.vinculado_or_no_frota(nrFrota) > 0:
+                erros.append("veiculo já vinculado")
+
+            if self.vinculado_or_no_motorista(nrMatricula) > 0:
+                erros.append("motorista já vinculado")
+
             if erros:
                 QMessageBox.critical(self, "Erro", "\n".join(erros))
                 return
 
+            print(f"Data vinculo: {dtVinculo} \n IDMOTORISTA: {id_motorista} \n IDVEICULO: {id_veiculo}")
             novo_vinculo = CtVinculo(DTVINCULO=dtVinculo, DTDESVINCULO=None,
                                      MOTORISTA_IDMOTORISTA=id_motorista, VEICULO_IDVEICULO=id_veiculo)
             session.add(novo_vinculo)
@@ -98,42 +144,13 @@ class VincularMotorista(QtWidgets.QMainWindow):
         except Exception as e:
             QMessageBox.critical(self, "Erro", f"Erro ao criar vínculo: {str(e)}")
 
-    def limpar_campos(self): #Função para limpar a tela ao terminar o cadastro
+    def limpar_campos(self):  # Função para limpar a tela ao terminar o cadastro
 
         self.dtVinculo.clear()
         self.nrMatricula.clear()
         self.nrFrota.clear()
 
-    def procurar_motorista(nrMatricula):
-        response = session.query(Motorista).join(Funcionario,Funcionario.NRMATRICULA == Motorista.FUNCIONARIO_NRMATRICULA).filter(Motorista.FUNCIONARIO_NRMATRICULA == nrMatricula).first()
-        id_motorista = response.IDMOTORISTA
-        return id_motorista
-
-    def procurar_frota(nrFrota):
-        response = session.query(Veiculo).filter(Veiculo.NRFROTA == nrFrota).first()
-        id_veiculo = response.IDVEICULO
-        return id_veiculo
-
-    def verificar_dt_desvinculo_nulo(self, id_veiculo, id_motorista):
-        # Verifica se existe algum vínculo ativo com o motorista e veículo especificados
-        vinculos = session.query(CtVinculo).filter(
-            CtVinculo.VEICULO_IDVEICULO == id_veiculo,
-            CtVinculo.MOTORISTA_IDMOTORISTA == id_motorista,
-            CtVinculo.DTDESVINCULO.is_(None)
-        ).count()
-        return vinculos > 0
-
-    '''def vinculado_or_no(self, nrMatricula, nrFrota):
-        id_motorista = VincularMotorista.procurar_motorista(nrMatricula)
-        id_veiculo = VincularMotorista.procurar_frota(nrFrota)
-
-        vinculos = session.query(CtVinculo.ID).filter(
-            CtVinculo.VEICULO_IDVEICULO == id_veiculo,
-            CtVinculo.MOTORISTA_IDMOTORISTA == id_motorista,
-            CtVinculo.DTDESVINCULO.is_(None)
-        ).count()
-
-        return vinculos == 0'''
+    # def verificar_data(self, dtVinculo):
 
     def chamar_tela_desvinculo(self):
         if not self.desvincula_view:
